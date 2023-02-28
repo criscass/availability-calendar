d680174a35281fcac58f9d40a8e12fcd4ed26496
Cristian Cassetta <cristiancassetta@gmail.com>
2023-02-28 06:13:09 +0100

    Add one event that shows on the calendar, in defined color

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,20 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+	import Calendar from '@event-calendar/core';
+	import Interaction from '@event-calendar/interaction';
+	import DayGrid from '@event-calendar/day-grid';
+	import '@event-calendar/core/index.css';
+
+	let plugins = [DayGrid, Interaction];
+	let options = {
+		view: 'dayGridMonth',
+		events: [{ title: 'Mr Smith', start: '2023-03-20', end: '2023-03-25' }],
+		eventColor: '#378006',
+		dateClick: function (info) {
+			console.log(info);
+		}
+	};
+</script>
+
+<h3>Availability "La Civettaia"</h3>
+
+<Calendar {plugins} {options} />
